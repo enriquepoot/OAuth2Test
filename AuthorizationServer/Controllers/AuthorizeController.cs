f84fe93616fb420d5394620fbaee8a148c6cc1e4
enrique.poot <enrique.poot@devfacto.com>
2014-09-29 22:06:51 -0500

    First Commit

---
+++ AuthorizationServer/Controllers/AuthorizeController.cs
@@ -0,0 +1,108 @@
+﻿using AuthorizationServer.Filters;
+using Data.Entities;
+using Data.Models;
+using Data.Respositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace AuthorizationServer.Controllers
+{
+    [Authorize]
+    [InitializeSimpleMembership]
+    public class AuthorizeController : Controller
+    {
+        private ClientRepository clientRepo = new ClientRepository();
+        private AuthorizationCodeRepository codeRepo = new AuthorizationCodeRepository();
+
+        //
+        // GET: /Authorize/
+        public ActionResult Index()
+        {
+            const string RESPONSE_TYPE = "response_type";
+            const string CLIENT_ID = "client_id";
+            const string REDIRECT_URI = "redirect_uri";
+            const string SCOPE = "scope";
+            const string STATE = "state";
+
+            var query = Request.QueryString;
+
+            AuthorizationModel model = new AuthorizationModel();
+            model.response_type = query[RESPONSE_TYPE];
+            model.client_id = query[CLIENT_ID];
+            model.redirect_uri = query[REDIRECT_URI];
+            model.scope = query[SCOPE]; //Optional
+            model.state = query[STATE];  //Recommended
+
+            if (string.IsNullOrEmpty(model.response_type) || string.IsNullOrEmpty(model.client_id) || string.IsNullOrEmpty(model.redirect_uri))
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "invalid_request"));
+
+            var client = clientRepo.GetClient(model.client_id);
+
+            if (client == null)
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "unauthorized_client"));
+
+            if (model.response_type != "code")
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "unsupported_response_type"));
+
+            //Agregar mas errores, especificados en el RFC6749
+
+            ViewBag.AppName = client.ClientName;
+
+            //Everything ok
+            return View(model);
+        }
+
+        //
+        // POST: /Authorize/
+        [HttpPost]
+        public ActionResult Index(AuthorizationModel model)
+        {
+            if (string.IsNullOrEmpty(model.response_type) || string.IsNullOrEmpty(model.client_id) || string.IsNullOrEmpty(model.redirect_uri))
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "invalid_request"));
+
+            var client = clientRepo.GetClient(model.client_id);
+
+            if (client == null)
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "unauthorized_client"));
+
+            if (model.response_type != "code")
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "unsupported_response_type"));
+
+            if(model.Deny)
+                return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "access_denied"));
+
+            //Agregar mas errores, especificados en el RFC6749
+
+            //Generate the code hash, valid for max (recommended) 10 minutes
+            var key = Convert.FromBase64String(client.ClientSecret);
+            var provider = new System.Security.Cryptography.HMACSHA256(key);
+
+            var UserID = WebSecurity.CurrentUserId;
+
+            var rawCodeInfo = string.Concat(client.ClientID, client.ClientSecret, UserID, DateTime.UtcNow.ToString("d"));
+            var rawCodeByte = Encoding.UTF8.GetBytes(rawCodeInfo);
+            var code = provider.ComputeHash(rawCodeByte);
+
+            var authorizationCode = new AuthorizationCode()
+            {
+                ClientID = model.client_id,
+                UserID = UserID,
+                Code = Convert.ToBase64String(code),
+                Expiration = DateTime.Now.AddMinutes(10),
+                Redirect_Uri = model.redirect_uri
+            };
+
+            if (codeRepo.Save(authorizationCode))
+                return Redirect(string.Format("{0}?code={1}", model.redirect_uri, HttpUtility.UrlEncode(authorizationCode.Code)));
+
+            return Redirect(string.Format("{0}?error={1}", model.redirect_uri, "server_error"));
+        }
+    }
+}
---
+++ AuthorizationServer/Controllers/TokenController.cs
@@ -0,0 +1,84 @@
+﻿using Data.Entities;
+using Data.Models;
+using Data.Respositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using WebMatrix.WebData;
+
+namespace AuthorizationServer.Controllers
+{
+    public class TokenController : Controller
+    {
+        private ClientRepository clientRepo = new ClientRepository();
+        private AuthorizationCodeRepository codeRepo = new AuthorizationCodeRepository();
+        private AccessTokenRepository tokenRepo = new AccessTokenRepository();
+
+        //
+        // POST: /Token/
+        [HttpPost]
+        public JsonResult Index(AuthorizationGrantModel model)
+        {
+            try
+            {
+                //Validate the code is used only once
+
+                var client = clientRepo.GetClient(model.client_id);
+
+                if (client == null)
+                    return Json(new { error = "invalid_request" }, JsonRequestBehavior.AllowGet);
+
+                if(!client.ClientSecret.Equals(model.client_secret))
+                    return Json(new { error = "invalid_client" }, JsonRequestBehavior.AllowGet);
+
+                var code = codeRepo.GetAuthorizationCode(model.code);
+
+                if(code == null)
+                    return Json(new { error = "invalid_grant" }, JsonRequestBehavior.AllowGet);
+
+                if(!code.ClientID.Equals(client.ClientID))
+                    return Json(new { error = "unauthorized_client" }, JsonRequestBehavior.AllowGet);
+
+                //Validate Uri match the client url registered
+
+                if(!string.IsNullOrEmpty(code.Redirect_Uri) && !code.Redirect_Uri.Equals(model.redirect_uri))
+                    return Json(new { error = "invalid_request" }, JsonRequestBehavior.AllowGet);
+
+                //Valid
+                var key = Convert.FromBase64String(client.ClientSecret);
+                var provider = new System.Security.Cryptography.HMACSHA256(key);
+
+                var UserID = WebSecurity.CurrentUserId;
+
+                var rawTokenInfo = string.Concat(code.Code, client.ClientSecret, UserID, DateTime.UtcNow.ToString("d"));
+                var rawTokenByte = Encoding.UTF8.GetBytes(rawTokenInfo);
+                var token = provider.ComputeHash(rawTokenByte);
+
+                var accessToken = new AccessToken()
+                {
+                    ClientID = model.client_id,
+                    UserID = UserID,
+                    Token = Convert.ToBase64String(token),
+                    Type = "bearer",
+                    Expiration = DateTime.Now.AddDays(1),
+                    RefreshToken = string.Empty,
+                    Scope = string.Empty
+                };
+
+                if (tokenRepo.Save(accessToken))
+                    return Json(new AccessTokenModel(accessToken), JsonRequestBehavior.AllowGet);
+
+            }
+            catch
+            {
+                return Json(new { error = "invalid_request" }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { error = "invalid_request" }, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
